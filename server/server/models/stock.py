746dd8be745ed9b31fc8fb7c3b4cde644c4a261e
jgabriel1 <josegabriel@det.ufc.br>
2020-06-25 20:25:31 -0300

    Changed Stocks serialization models to accommodate the new fields.

--- server/server/models/stock.py
+++ server/server/models/stock.py
@@ -5,25 +5,23 @@ from pydantic import BaseModel, validator
 
 class Stock(BaseModel):
     ticker: str
-    price_bought: float
-    quantity: int
-
-    total_value: float = None
+    total_invested: float
+    total_sold: float
+    currently_owned_shares: int
+    average_bought_price: float
 
     @validator('ticker')
     def uppercase_any_ticker(cls, value: str):
         return value.upper()
 
-    @validator('total_value', always=True)
-    def calculate_total_invested(cls, value, values, **kwargs):
-        return values['price_bought'] * values['quantity']
-
 
 class StockOutInfo(BaseModel):
     # From Stock model:
     ticker: str
-    quantity: int
-    total_value: float
+    total_invested: float
+    average_bought_price: float
+    total_sold: float
+    currently_owned_shares: int
 
     # From Yahoo Finance stock model:
     regularMarketPrice: float
@@ -32,3 +30,12 @@ class StockOutInfo(BaseModel):
 
 class StocksResponse(BaseModel):
     stocks: List[StockOutInfo]
+    total_applied: float = None
+
+    @validator('total_applied', always=True)
+    def calculate_applied(cls, value, values, **kwargs):
+        stocks = values['stocks']
+        return sum(
+            stock.currently_owned_shares * stock.average_bought_price
+            for stock in stocks
+        )
