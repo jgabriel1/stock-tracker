196112d8932a3b446b2d01be191c2cae46f8b915
jgabriel1 <josegabriel@det.ufc.br>
2020-06-28 23:35:45 -0300

    Added frontend url as an environment variable.

--- server/server/main.py
+++ server/server/main.py
@@ -1,12 +1,20 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from .api import routes
+
+load_dotenv()
+
+FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:3000')
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=[FRONTEND_URL],
     allow_methods=['*'],
     allow_headers=['*'],
     allow_credentials=True
