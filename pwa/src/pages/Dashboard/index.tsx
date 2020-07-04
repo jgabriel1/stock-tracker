d2208f3ba19996556af848a543ca411b64380b73
jgabriel1 <josegabriel@det.ufc.br>
2020-07-04 16:04:14 -0300

    Auth token being read correctly.

--- pwa/src/pages/Dashboard/index.tsx
+++ pwa/src/pages/Dashboard/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { getAuthToken } from '../../utils/tokenHandler'
+import api from '../../services/api'
+
 const Dashboard = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+
+    useEffect(() => {
+        getAuthToken()
+            .then(token => {
+                const headers = { Authorization: `Bearer ${token}` }
+
+                api.get('users/me', { headers })
+                    .then(response => {
+                        const { username, email } = response.data
+
+                        setUsername(username)
+                        setEmail(email)
+                    })
+                    .catch(error => alert(error))
+            })
+            .catch(error => alert(error))
+    }, [])
+
     return (
-        <View>
-            <Text>This is the dashboard</Text>
+        <View style={styles.container}>
+            <Text>Username: {username}</Text>
+            <Text>Email: {email}</Text>
         </View>
     )
 }
 
 export default Dashboard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
