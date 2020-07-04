e872fecb1493885e4d79bd3d0aa3ab521803c3cd
jgabriel1 <josegabriel@det.ufc.br>
2020-07-04 20:01:18 -0300

    Created registration form.

--- pwa/src/pages/Register/index.tsx
+++ pwa/src/pages/Register/index.tsx
@@ -1,14 +1,85 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View, TextInput } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import Button from '../../components/Button'
+
+import api from '../../services/api'
 
 const Register = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigation = useNavigation()
+
+    function handleSubmitRegistration() {
+        const data = {
+            username,
+            email,
+            password
+        }
+
+        api.post('auth/register', data)
+            .then(() => {
+                navigation.navigate('Login')
+
+                setUsername('')
+                setEmail('')
+                setPassword('')
+            })
+            .catch(error => alert(error))
+    }
+
     return (
-        <View>
-            <Text>Register page</Text>
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                placeholder={'Username'}
+                value={username}
+                onChangeText={text => setUsername(text)}
+                autoCapitalize={'none'}
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder={'Email'}
+                value={email}
+                onChangeText={text => setEmail(text)}
+                autoCapitalize={'none'}
+                keyboardType={'email-address'}
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder={'Password'}
+                value={password}
+                onChangeText={text => setPassword(text)}
+                autoCapitalize={'none'}
+                secureTextEntry={true}
+            />
+
+            <Button text={'Register'} onPress={handleSubmitRegistration} />
         </View>
     )
 }
 
 export default Register
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#aaa',
+        borderRadius: 4,
+        padding: 16,
+        width: '80%',
+        fontSize: 16,
+        marginBottom: 16
+    },
+})
