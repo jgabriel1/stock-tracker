cb58b75431d689b5c045addea225d8c8256bfd32
jgabriel1 <josegabriel@det.ufc.br>
2020-07-04 10:44:29 -0300

    Created Landing page

--- pwa/src/pages/Home/index.tsx
+++ pwa/src/pages/Home/index.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 
 const Home = () => {
+    const navigation = useNavigation()
+
+    function navigateToLogin() {
+        navigation.navigate('Login')
+    }
+
+    function navigateToRegister() {
+        navigation.navigate('Register')
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Hello from routes</Text>
+            <View style={styles.titleContainer}>
+                <Text style={styles.titleText}>Stock Tracker</Text>
+                <Text style={styles.titleDescription}>An app to track the stocks you own and project your gains.</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={navigateToLogin}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={navigateToRegister}>
+                    <Text style={styles.buttonText}>Register</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 export default Home
-
 
 const styles = StyleSheet.create({
     container: {
@@ -19,6 +40,41 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 8
     },
+
+    titleContainer: {
+        width: '80%',
+        paddingBottom: 96
+    },
+
+    titleText: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        paddingBottom: 32
+    },
+
+    titleDescription: {
+        fontSize: 14,
+    },
+
+    buttonContainer: {
+        width: '80%',
+    },
+
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#999',
+        height: 48,
+        borderRadius: 4,
+        marginBottom: 16
+    },
+
+    buttonText: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#fff',
+    }
 })
 
