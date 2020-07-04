610a9919a36eecd7ce7e590be7c202a52eae69e6
jgabriel1 <josegabriel@det.ufc.br>
2020-07-04 14:15:49 -0300

    Changed to run server on local network instead of localhost.

--- server/scripts.py
+++ server/scripts.py
@@ -4,14 +4,30 @@ from subprocess import call
 def start():
     print('INFO:     Starting Server...')
     try:
-        call(['uvicorn', 'server.main:app', '--no-use-colors'])
+        call([
+            'uvicorn',
+            'server.main:app',
+            '--host', '192.168.2.2',
+            '--port', '8000',
+            '--no-use-colors'
+        ])
     except KeyboardInterrupt:
         print('INFO:     Keyboard Interrupt.')
 
 
 def dev():
     print('INFO:     Starting Server... [Debug Mode]')
-    call(['uvicorn', 'server.main:app', '--reload', '--no-use-colors'])
+    try:
+        call([
+            'uvicorn',
+            'server.main:app',
+            '--host', '192.168.2.2',
+            '--port', '8000',
+            '--reload',
+            '--no-use-colors'
+        ])
+    except KeyboardInterrupt:
+        print('INFO:     Keyboard Interrupt.')
 
 
 def test():
