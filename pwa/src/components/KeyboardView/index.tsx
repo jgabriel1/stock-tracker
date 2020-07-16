bb39ffd9d0b7f61109b1a86017785f328e580a82
jgabriel1 <josegabriel@det.ufc.br>
2020-07-16 19:38:30 -0300

    Changed KeyboardView to be wrapped by a SafeAreaView and recieve style props.

--- pwa/src/components/KeyboardView/index.tsx
+++ pwa/src/components/KeyboardView/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import { Keyboard, TouchableWithoutFeedback } from 'react-native'
+import { Keyboard, TouchableWithoutFeedback, ViewStyle } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+interface Props {
+    style?: ViewStyle
+}
 
 
-const KeyboardView: React.FC = ({ children }) => (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <KeyboardAwareScrollView
-            alwaysBounceVertical={false}
-            contentContainerStyle={{ flex: 1 }}
-            showsVerticalScrollIndicator={false}
-        >
-            {children}
-        </KeyboardAwareScrollView>
-    </TouchableWithoutFeedback>
+const KeyboardView: React.FC<Props> = ({ children, style }) => (
+    <SafeAreaView style={{ flex: 1, ...style }}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
+            <KeyboardAwareScrollView
+                alwaysBounceVertical={false}
+                contentContainerStyle={{ flex: 1 }}
+                showsVerticalScrollIndicator={false}
+            >
+                {children}
+            </KeyboardAwareScrollView>
+
+        </TouchableWithoutFeedback>
+    </SafeAreaView>
 )
 
 
