3583ab947c412b238102619549f0bef334ccb423
jgabriel1 <josegabriel@det.ufc.br>
2020-07-24 22:04:56 -0300

    Added sketch of data presentation and increase/decrease buttons.

--- pwa/src/pages/Detail/index.tsx
+++ pwa/src/pages/Detail/index.tsx
@@ -6,10 +6,14 @@ import { AppLoading } from 'expo'
 import ReturnButton from '../../components/ReturnButton'
 
 import API from '../../services/api'
-import { Transaction } from '../../services/api/types'
+import { Transaction, Stock } from '../../services/api/types'
 import DataContext from '../../store/dataContext'
 
 import { AppStackParamList } from '../../routes'
+import { Provider } from 'react-native-paper'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Feather as Icon } from '@expo/vector-icons'
+import { YahooStock } from '../../services/yahooFinance/stockInfo'
 
 
 const Detail = () => {
@@ -21,43 +25,90 @@ const Detail = () => {
     const { state, dispatch } = useContext(DataContext)
     const { stocksData, isStocksDataReady, yahooData, isYahooDataReady } = state
 
+    const { currently_owned_shares, average_bought_price } = stocksData.get(ticker) as Stock
+    const { regularMarketPrice } = yahooData.get(ticker) as YahooStock
+
+    const totalInvested = currently_owned_shares * average_bought_price
+    const currentWorth = currently_owned_shares * regularMarketPrice
+    const balance = currentWorth - totalInvested
+    const variation = 100 * (regularMarketPrice / average_bought_price - 1)
+
     useEffect(() => {
-        API.getTransactionsFor(ticker)
-            .then(setTransactionList)
+        API.getTransactionsFor(ticker).then(setTransactionList)
     }, [])
+
+    async function handleSellStock() { }
 
     if (!isStocksDataReady || !isYahooDataReady) {
         return <AppLoading />
     }
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
+        <Provider>
+            <SafeAreaView style={{ flex: 1 }}>
 
-            <ReturnButton />
+                <ReturnButton />
 
-            <View style={styles.container}>
+                <View style={styles.container}>
 
-                <View style={styles.titleContainer}>
-                    <Text style={styles.title}>{ticker}</Text>
-                </View>
-
-                <View style={styles.infoContainer}>
-                    <Text>{yahooData.get(ticker)?.symbol}</Text>
-                    <Text>{yahooData.get(ticker)?.regularMarketPrice}</Text>
-                    <Text>{yahooData.get(ticker)?.chartPreviousClose}</Text>
-                </View>
-
-                {transactionList.map((transaction, index) => (
-                    <View style={styles.transactionContainer} key={index}>
-                        <Text>{transaction.quantity}</Text>
-                        <Text>{transaction.average_price}</Text>
-                        <Text>{transaction.total_value}</Text>
-                        <Text>{transaction.timestamp}</Text>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>{ticker}</Text>
                     </View>
-                ))}
 
-            </View>
-        </SafeAreaView>
+                    <View style={styles.infoContainer}>
+                        <View style={styles.infoRow}>
+                            <View style={styles.infoItem}>
+                                <Text style={styles.infoItemTitle}>Total Invested:</Text>
+                                <Text style={styles.infoItemValue}>{`$ ${totalInvested.toFixed(2)}`}</Text>
+                            </View>
+
+                            <View style={styles.infoItem}>
+                                <Text style={styles.infoItemTitle}>Current Worth:</Text>
+                                <Text style={styles.infoItemValue}>{`$ ${currentWorth.toFixed(2)}`}</Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.infoRow}>
+                            <View style={styles.infoItem}>
+                                <Text style={styles.infoItemTitle}>Balance:</Text>
+                                <Text style={styles.infoItemValue}>{`$ ${balance.toFixed(2)}`}</Text>
+                            </View>
+
+                            <View style={styles.infoItem}>
+                                <Text style={styles.infoItemTitle}>Variation(%):</Text>
+                                <Text style={styles.infoItemValue}>{`${variation.toFixed(2)} %`}</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={styles.buttonsContainer}>
+                        <TouchableOpacity
+                            onPress={() => console.log('buy')}
+                            containerStyle={{ ...styles.button, marginRight: 4 }}
+                        >
+                            <Icon name='plus' size={32} color='#eee' />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => console.log('sell')}
+                            containerStyle={{ ...styles.button, marginLeft: 4 }}
+                        >
+                            <Icon name='minus' size={32} color='#eee' />
+                        </TouchableOpacity>
+                    </View>
+
+                    {transactionList.map((transaction, index) => (
+                        <View style={styles.transactionContainer} key={index}>
+                            <Text>{transaction.quantity}</Text>
+                            <Text>{transaction.average_price}</Text>
+                            <Text>{transaction.total_value}</Text>
+                            <Text>{new Date((transaction.timestamp as number) * 1000).toISOString()}</Text>
+                        </View>
+                    ))}
+
+                </View>
+            </SafeAreaView>
+        </Provider>
     )
 }
 
@@ -71,7 +122,8 @@ const styles = StyleSheet.create({
     },
 
     titleContainer: {
-        width: '80%'
+        width: '80%',
+        marginBottom: 16,
     },
 
     title: {
@@ -80,8 +132,52 @@ const styles = StyleSheet.create({
     },
 
     infoContainer: {
-        width: '80%',
+        width: '100%',
+        marginBottom: 8,
+        paddingHorizontal: 8,
+    },
 
+    infoRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 8
+    },
+
+    infoItem: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 0.5,
+        borderRadius: 4,
+        borderColor: '#999',
+        paddingVertical: 8,
+        paddingHorizontal: 4,
+        marginHorizontal: 4,
+    },
+
+    infoItemTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        paddingBottom: 4,
+    },
+
+    infoItemValue: {
+        fontSize: 16,
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        width: '80%',
+    },
+
+    button: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 48,
+        borderRadius: 4,
+        marginBottom: 16,
+        backgroundColor: '#999',
     },
 
     transactionContainer: {
