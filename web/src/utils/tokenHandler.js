86b5a6453a57d2f188766972192485226e1ab77b
jgabriel1 <josegabriel@det.ufc.br>
2020-07-30 14:40:53 -0300

    Deleted all files from the old React Web application.

--- web/src/utils/tokenHandler.js
+++
@@ -1,8 +0,0 @@
-export const setToken = token => {
-    sessionStorage.setItem('token', token)
-}
-
-export const getToken = () => {
-    const token = sessionStorage.getItem('token')
-    return token
-}
