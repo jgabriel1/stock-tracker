f80c68565ee1e445b784c6bfe80bb11c62096f04
jgabriel1 <josegabriel@det.ufc.br>
2020-08-08 23:49:24 -0300

    Got rid of old tests.

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -4,16 +4,19 @@ from fastapi.testclient import TestClient
 from server.api.dependencies import get_db
 from server.main import app
 
-from .database import get_test_db, mongo_client
+from .settings.database import get_test_db, mongo_client
 
 
 @pytest.fixture(scope='session', autouse=True)
 def override_test_db():
     app.dependency_overrides[get_db] = get_test_db
+    yield
 
 
 @pytest.fixture(scope='function', autouse=True)
 def reset_db():
+    yield
+
     test_db = mongo_client.get_database()
 
     test_db.drop_collection('users')
