3145440be2a2cd749e4b84fa3e0b500b93459f32
jgabriel1 <josegabriel@det.ufc.br>
2020-08-22 19:38:13 -0300

    Styled bottom tabs.

--- pwa/src/routes/DashboardTabs.tsx
+++ pwa/src/routes/DashboardTabs.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Platform } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather as Icon } from '@expo/vector-icons'
 
 import MainDashboard from '../pages/MainDashboard'
 import StocksList from '../pages/StocksList'
@@ -8,9 +10,52 @@ const { Navigator, Screen } = createBottomTabNavigator()
 
 const DashboardTabs = () => {
     return (
-        <Navigator>
-            <Screen name='MainDashboard' component={MainDashboard} />
-            <Screen name='StocksList' component={StocksList} />
+        <Navigator
+            tabBarOptions={{
+                style: {
+                    elevation: 0,
+                    shadowOpacity: 0,
+                },
+                tabStyle: {
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                },
+                iconStyle: {
+                    flex: 0,
+                    width: 20,
+                    height: Platform.OS === 'ios' ? 24 : 20,
+                },
+                labelStyle: {
+                    fontSize: 13,
+                    marginLeft: 16,
+                },
+                inactiveBackgroundColor: '#fafafc',
+                activeBackgroundColor: '#ebebeb',
+                inactiveTintColor: '#cccccc',
+                activeTintColor: '#32264d',
+            }}
+        >
+            <Screen
+                name='MainDashboard'
+                component={MainDashboard}
+                options={{
+                    tabBarLabel: 'Dashboard',
+                    tabBarIcon: ({ size }) => (
+                        <Icon name='monitor' size={size} />
+                    )
+                }}
+            />
+            <Screen
+                name='StocksList'
+                component={StocksList}
+                options={{
+                    tabBarLabel: 'Stocks List',
+                    tabBarIcon: ({ size }) => (
+                        <Icon name='list' size={size} />
+                    )
+                }}
+            />
         </Navigator>
     )
 }
