f40a296c9dbed469e6ec8954051c7ee6116932b1
jgabriel1 <josegabriel@det.ufc.br>
2020-09-10 19:46:06 -0300

    Formatted components and hooks.

--- pwa/src/components/FourBoxGrid/styles.ts
+++ pwa/src/components/FourBoxGrid/styles.ts
@@ -1,39 +1,39 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-    outterContainer: {
-        flex: 1,
-        margin: 16,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-    },
+  outterContainer: {
+    flex: 1,
+    margin: 16,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
 
-    innerContainer: {
-        width: '50%',
-        height: '50%',
-        marginBottom: -0.25,
-        borderColor: '#ccc',
-    },
+  innerContainer: {
+    width: '50%',
+    height: '50%',
+    marginBottom: -0.25,
+    borderColor: '#ccc',
+  },
 
-    topLeft: {
-        borderRightWidth: 0.5,
-        borderBottomWidth: 0.5,
-    },
+  topLeft: {
+    borderRightWidth: 0.5,
+    borderBottomWidth: 0.5,
+  },
 
-    topRight: {
-        borderBottomWidth: 0.5,
-        borderLeftWidth: 0.5,
-    },
+  topRight: {
+    borderBottomWidth: 0.5,
+    borderLeftWidth: 0.5,
+  },
 
-    bottomLeft: {
-        borderTopWidth: 0.5,
-        borderRightWidth: 0.5,
-    },
+  bottomLeft: {
+    borderTopWidth: 0.5,
+    borderRightWidth: 0.5,
+  },
 
-    bottomRight: {
-        borderLeftWidth: 0.5,
-        borderTopWidth: 0.5,
-    },
+  bottomRight: {
+    borderLeftWidth: 0.5,
+    borderTopWidth: 0.5,
+  },
 })
 
 export default styles
