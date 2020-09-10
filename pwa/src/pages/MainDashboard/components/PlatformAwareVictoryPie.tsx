3a914acb9355a9a924652596777e97fb1ead55ad
jgabriel1 <josegabriel@det.ufc.br>
2020-09-10 15:43:36 -0300

    Fixed chart error while running on ios physical device.

--- pwa/src/pages/MainDashboard/components/PlatformAwareVictoryPie.tsx
+++ pwa/src/pages/MainDashboard/components/PlatformAwareVictoryPie.tsx
@@ -3,7 +3,7 @@ import { Platform } from 'react-native'
 import { VictoryPie as WebVictoryPie } from 'victory'
 import { VictoryPie as NativeVictoryPie } from 'victory-native'
 
-export default class PlatformAwareVictoryPie extends WebVictoryPie {
+export default class PlatformAwareVictoryPie extends NativeVictoryPie {
     render() {
         return Platform.OS === 'web'
             ? <WebVictoryPie {...this.props} />
