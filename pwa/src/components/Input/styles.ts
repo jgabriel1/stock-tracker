b54becdd6061cdaaf242c9d493267d2dae271cd3
jgabriel1 <josegabriel@det.ufc.br>
2020-10-09 17:11:28 -0300

    Created simple input component.

---
+++ pwa/src/components/Input/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.TextInput`
+  width: 80%;
+  height: 64px;
+  border: 1px solid #a2a2a2;
+
+  padding: 16px 24px;
+  margin-bottom: 18px;
+  border-radius: 24px;
+  font-size: 16px;
+`
