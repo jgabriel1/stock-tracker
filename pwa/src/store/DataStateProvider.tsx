24d4385248597024fb2bb876c1954c8429492bd4
jgabriel1 <josegabriel@det.ufc.br>
2020-10-19 21:06:42 -0300

    Got rid of old global state store in favor of the new hooks.

--- pwa/src/store/DataStateProvider.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import useDataState from './useDataState'
-import DataContext from './dataContext'
-
-const DataStateProvider: React.FC = ({ children }) => (
-  <DataContext.Provider value={useDataState()}>{children}</DataContext.Provider>
-)
-
-export default DataStateProvider
