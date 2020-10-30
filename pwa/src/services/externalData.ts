d7158a7f2a077db28c786810edfefc948a196e7d
jgabriel1 <josegabriel@det.ufc.br>
2020-10-30 14:47:45 -0300

    Abstracted the external data http client access into a hook.

--- pwa/src/services/externalData.ts
+++ pwa/src/services/externalData.ts
@@ -2,8 +2,59 @@ import axios from 'axios'
 
 import { EXTERNAL_DATA_CLIENT } from './config'
 
-const externalData = axios.create({
+interface IExternalSearch {
+  symbol: string
+  exchange: string
+  longname?: string
+  typeDisp?: string
+}
+
+interface IExternalSearchResponse {
+  data: Array<IExternalSearch>
+}
+
+interface IExternalInfo {
+  symbol: string
+  currency: string
+  regularMarketPrice: number
+  chartPreviousClose: number
+}
+
+interface IExternalInfoResponse {
+  data: Map<string, IExternalInfo>
+}
+
+const client = axios.create({
   baseURL: EXTERNAL_DATA_CLIENT,
 })
 
-export default externalData
+async function getSearch(query: string): Promise<IExternalSearch[]> {
+  const params = {
+    query,
+  }
+
+  const response = await client.get<IExternalSearchResponse>('search', {
+    params,
+  })
+
+  return response.data.data
+}
+
+async function getInfo(tickers: string[]): Promise<Map<string, IExternalInfo>> {
+  const params = {
+    tickers: tickers.map(ticker => ticker.toUpperCase()).join(','),
+  }
+
+  const response = await client.get<IExternalInfoResponse>('info', {
+    params,
+  })
+
+  return response.data.data
+}
+
+export default function useExternalData() {
+  return {
+    getSearch,
+    getInfo,
+  }
+}
