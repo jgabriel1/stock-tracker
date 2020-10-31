794be138860f5190bfe9eec287602e8c7ca2e5f7
jgabriel1 <josegabriel@det.ufc.br>
2020-10-31 13:10:24 -0300

    Removed the old yahooFinance module to access external data and parse it.

--- pwa/src/services/yahooFinance/urls.ts
+++
@@ -1,3 +0,0 @@
-export const BASE_API_URL = 'https://query1.finance.yahoo.com/'
-export const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
-export const BACKEND_PROXY_URL = 'http://stock-tracker-backend.herokuapp.com/'
