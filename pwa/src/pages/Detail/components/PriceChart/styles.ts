c042e29cf42afcd9b8b7511207df0a7a53eb8d6e
jgabriel1 <josegabriel@det.ufc.br>
2020-11-20 15:46:19 -0300

    Created the range toggling buttons styles.

--- pwa/src/pages/Detail/components/PriceChart/styles.ts
+++ pwa/src/pages/Detail/components/PriceChart/styles.ts
@@ -1,10 +1,51 @@
 import { Dimensions } from 'react-native'
-import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+import styled, { css } from 'styled-components/native'
+
+interface RangeButtonProps {
+  active: boolean
+}
 
 const CHART_WIDTH = Dimensions.get('window').width - 60
 
 export const Container = styled.View`
-  width: ${Dimensions.get('window').width - 60}px;
+  width: ${CHART_WIDTH}px;
+`
+
+export const RangeButtonsListContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  margin-bottom: 4px;
+  height: 40px;
+`
+
+export const RangeButton = styled(RectButton)`
+  flex: 1;
+`
+
+export const RangeButtonContainer = styled.View<RangeButtonProps>`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  ${props =>
+    props.active &&
+    css`
+      border-bottom-width: 2px;
+      margin-bottom: -2px;
+    `}
+`
+
+export const RangeButtonText = styled.Text<RangeButtonProps>`
+  font-size: 16px;
+  opacity: 0.3;
+
+  ${props =>
+    props.active &&
+    css`
+      opacity: 1;
+    `}
 `
 
 export const PlaceholderTextContainer = styled.View`
