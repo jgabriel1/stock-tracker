26c1de268ff6e3014ef986b62b3aa7f6021b9577
jgabriel1 <josegabriel@det.ufc.br>
2020-11-20 15:46:59 -0300

    Added the chart range fetching logic.

--- pwa/src/pages/Detail/components/PriceChart/index.tsx
+++ pwa/src/pages/Detail/components/PriceChart/index.tsx
@@ -1,10 +1,18 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { Dimensions } from 'react-native'
 import { VictoryAxis, VictoryChart, VictoryLine } from 'victory-native'
 
 import useExternalData from '../../../../services/externalData'
 
-import { Container, PlaceholderText, PlaceholderTextContainer } from './styles'
+import {
+  Container,
+  RangeButtonsListContainer,
+  RangeButton,
+  RangeButtonText,
+  PlaceholderText,
+  PlaceholderTextContainer,
+  RangeButtonContainer,
+} from './styles'
 
 interface PriceChartProps {
   ticker: string
@@ -15,10 +23,26 @@ interface ChartData {
   close: number[]
 }
 
+type ChartRange = '1d' | '5d' | '1mo' | '6mo' | 'YTD'
+
+type ChartRangeOptions = Array<{
+  key: string
+  value: ChartRange
+}>
+
 const CHART_WIDTH = Dimensions.get('window').width - 60
+
+const CHART_RANGE_OPTIONS: ChartRangeOptions = [
+  { key: '1d', value: '1d' },
+  { key: '5d', value: '5d' },
+  { key: '1mo', value: '1mo' },
+  { key: '6mo', value: '6mo' },
+  { key: 'YTD', value: 'YTD' },
+]
 
 const PriceChart: React.FC<PriceChartProps> = ({ ticker }) => {
   const [chartData, setChartData] = useState<ChartData | null>(null)
+  const [chartRange, setChartRange] = useState<ChartRange>('1d')
 
   const { getChartData } = useExternalData()
 
@@ -35,68 +59,80 @@ const PriceChart: React.FC<PriceChartProps> = ({ ticker }) => {
   }, [chartData])
 
   useEffect(() => {
-    getChartData(ticker).then(setChartData)
+    getChartData(ticker, { range: chartRange }).then(setChartData)
 
     return () => {
       setChartData(null)
     }
-  }, [getChartData, ticker])
-
-  if (!chartData) {
-    return (
-      <PlaceholderTextContainer>
-        <PlaceholderText>Loading Chart...</PlaceholderText>
-      </PlaceholderTextContainer>
-    )
-  }
+  }, [chartRange, getChartData, ticker])
 
   return (
     <Container>
-      <VictoryChart
-        width={CHART_WIDTH}
-        padding={{
-          top: 20,
-          bottom: 40,
-          right: 20,
-          left: 50,
-        }}
-        scale={{ x: 'time', y: 'linear' }}
-      >
-        <VictoryLine interpolation="basis" data={parsedData || undefined} />
-
-        <VictoryAxis
-          tickCount={5}
-          fixLabelOverlap
-          style={{
-            tickLabels: {
-              fontSize: 12,
-            },
-            grid: {
-              stroke: 'rgba(100,100,100,0.20)',
-              strokeWidth: 1,
-            },
-            ticks: {
-              stroke: 'grey',
-              size: 5,
-            } as React.CSSProperties,
+      <RangeButtonsListContainer>
+        {CHART_RANGE_OPTIONS.map(({ key, value }) => (
+          <RangeButton key={key}>
+            <RangeButtonContainer active={chartRange === value}>
+              <RangeButtonText
+                onPress={() => setChartRange(value)}
+                active={chartRange === value}
+              >
+                {key}
+              </RangeButtonText>
+            </RangeButtonContainer>
+          </RangeButton>
+        ))}
+      </RangeButtonsListContainer>
+      {chartData ? (
+        <VictoryChart
+          width={CHART_WIDTH}
+          padding={{
+            top: 20,
+            bottom: 40,
+            right: 20,
+            left: 50,
           }}
-        />
+          scale={{ x: 'time', y: 'linear' }}
+        >
+          <VictoryLine interpolation="basis" data={parsedData || undefined} />
 
-        <VictoryAxis
-          dependentAxis
-          tickCount={5}
-          fixLabelOverlap
-          style={{
-            axis: {
-              stroke: 'none',
-            },
-            grid: {
-              stroke: 'rgba(100,100,100,0.20)',
-              strokeWidth: 1,
-            },
-          }}
-        />
-      </VictoryChart>
+          <VictoryAxis
+            tickCount={5}
+            fixLabelOverlap
+            style={{
+              tickLabels: {
+                fontSize: 12,
+              },
+              grid: {
+                stroke: 'rgba(100,100,100,0.20)',
+                strokeWidth: 1,
+              },
+              ticks: {
+                stroke: 'grey',
+                size: 5,
+              } as React.CSSProperties,
+            }}
+          />
+
+          <VictoryAxis
+            dependentAxis
+            tickCount={5}
+            fixLabelOverlap
+            style={{
+              axis: {
+                stroke: 'none',
+              },
+              grid: {
+                stroke: 'rgba(100,100,100,0.20)',
+                strokeWidth: 1,
+              },
+            }}
+          />
+        </VictoryChart>
+      ) : (
+        <PlaceholderTextContainer>
+          <PlaceholderText>Loading Chart...</PlaceholderText>
+        </PlaceholderTextContainer>
+      )}
     </Container>
   )
 }
