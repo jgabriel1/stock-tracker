f78e95880952a4f2be7da467fe8057d138ed54f7
jgabriel1 <josegabriel@det.ufc.br>
2020-11-22 08:08:48 -0300

    Created context that provides any number of Cache instances throughout the App.

---
+++ pwa/src/hooks/cache.tsx
@@ -0,0 +1,98 @@
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
+import { Cache } from 'react-native-cache'
+
+interface CacheContextData {
+  caches: Map<string, Cache>
+}
+
+const CacheContext = createContext<CacheContextData>({} as CacheContextData)
+
+export const CacheProvider: React.FC = ({ children }) => {
+  const caches = useRef(new Map<string, Cache>())
+
+  return (
+    <CacheContext.Provider
+      value={{
+        caches: caches.current,
+      }}
+    >
+      {children}
+    </CacheContext.Provider>
+  )
+}
+
+function useCache(namespace: string) {
+  const { caches } = useContext(CacheContext)
+
+  if (!caches.has(namespace)) {
+    caches.set(
+      namespace,
+      new Cache({
+        namespace,
+        backend: AsyncStorage,
+        policy: {
+          maxEntries: 10,
+          stdTTL: 30,
+        } as { maxEntries: number },
+      }),
+    )
+  }
+
+  const cache = caches.get(namespace) as Cache
+
+  return cache
+}
+
+export function useCachedItem<T>(
+  namespace: string,
+  buildKey: (...keyDependencies: React.DependencyList) => string,
+  keyDependencies: React.DependencyList = [],
+): [T | null | undefined, (data: T) => Promise<void>] {
+  const cache = useCache(namespace)
+
+  const [key, setKey] = useState(() => buildKey(...keyDependencies))
+  const [value, setValue] = useState<T | null>()
+
+  const loadData = useCallback(async () => {
+    const storedValue = await cache.get(key)
+
+    if (!storedValue) {
+      setValue(null)
+
+      return
+    }
+
+    const parsedValue = JSON.parse(storedValue) as T
+
+    setValue(parsedValue)
+  }, [cache, key])
+
+  const storeData = useCallback(
+    async (data: T) => {
+      const stringifiedValue = JSON.stringify(data)
+
+      await cache.set(key, stringifiedValue)
+
+      setValue(data)
+    },
+    [cache, key],
+  )
+
+  useEffect(() => {
+    setKey(() => buildKey(...keyDependencies))
+  }, [buildKey, keyDependencies, loadData])
+
+  useEffect(() => {
+    loadData()
+  }, [loadData])
+
+  return [value, storeData]
+}
