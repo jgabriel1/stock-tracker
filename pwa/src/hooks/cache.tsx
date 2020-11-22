15585836f417d325b1f9e865df4b77e1f96decdd
jgabriel1 <josegabriel@det.ufc.br>
2020-11-22 16:59:27 -0300

    Fix: Implemented my own ttl check since the cache's doesn't appear to be working.

--- pwa/src/hooks/cache.tsx
+++ pwa/src/hooks/cache.tsx
@@ -8,9 +8,19 @@ import React, {
 } from 'react'
 import AsyncStorage from '@react-native-community/async-storage'
 import { Cache } from 'react-native-cache'
+import { addSeconds, isAfter } from 'date-fns'
 
 interface CacheContextData {
   caches: Map<string, Cache>
+}
+
+interface StoredValue<V> {
+  data: V
+  expiresAt: Date
+}
+
+interface CacheOptions {
+  ttlInSeconds: number
 }
 
 const CacheContext = createContext<CacheContextData>({} as CacheContextData)
@@ -55,6 +65,7 @@ export function useCachedItem<T>(
   namespace: string,
   buildKey: (...keyDependencies: React.DependencyList) => string,
   keyDependencies: React.DependencyList = [],
+  { ttlInSeconds }: CacheOptions = { ttlInSeconds: 15 },
 ): [T | null | undefined, (data: T) => Promise<void>] {
   const cache = useCache(namespace)
 
@@ -70,20 +81,34 @@ export function useCachedItem<T>(
       return
     }
 
-    const parsedValue = JSON.parse(storedValue) as T
+    const { data, expiresAt } = JSON.parse(storedValue) as StoredValue<T>
 
-    setValue(parsedValue)
+    const isExpired = isAfter(Date.now(), new Date(expiresAt))
+    if (isExpired) {
+      await cache.remove(key)
+
+      setValue(null)
+
+      return
+    }
+
+    setValue(data)
   }, [cache, key])
 
   const storeData = useCallback(
     async (data: T) => {
-      const stringifiedValue = JSON.stringify(data)
+      const expiresAt = addSeconds(Date.now(), ttlInSeconds)
+
+      const stringifiedValue = JSON.stringify({
+        data,
+        expiresAt,
+      })
 
       await cache.set(key, stringifiedValue)
 
       setValue(data)
     },
-    [cache, key],
+    [cache, key, ttlInSeconds],
   )
 
   useEffect(() => {
