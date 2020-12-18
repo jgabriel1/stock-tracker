50523c6cdc5daa74cf66d323fb7d17f2fd5bc106
jgabriel1 <josegabriel@det.ufc.br>
2020-12-18 19:03:56 -0300

    Added provider to the app.

--- pwa/src/index.tsx
+++ pwa/src/index.tsx
@@ -4,18 +4,21 @@ import { NavigationContainer } from '@react-navigation/native'
 
 import Routes from './routes'
 import AppProvider from './hooks'
+import { DatabaseConnectionProvider } from './data/connection'
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <AppProvider>
-        <StatusBar
-          barStyle="dark-content"
-          backgroundColor="#ededed"
-          translucent
-        />
-        <Routes />
-      </AppProvider>
+      <DatabaseConnectionProvider>
+        <AppProvider>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="#ededed"
+            translucent
+          />
+          <Routes />
+        </AppProvider>
+      </DatabaseConnectionProvider>
     </NavigationContainer>
   )
 }
